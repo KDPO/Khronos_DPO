60d60e636dd9f732b7511652b710a00eef7208d3
markostijak <marko.stijak@gmail.com>
2015-12-12 18:36:27 +0100

    Initial commit

---
+++ src/net/etfbl/kdpo/client/Main.java
@@ -0,0 +1,7 @@
+package net.etfbl.kdpo.client;
+
+/**
+ * Created by Stijak on 12.12.2015..
+ */
+public class Main {
+}
---
+++ src/net/etfbl/kdpo/server/ServerThread.java
@@ -0,0 +1,7 @@
+package net.etfbl.kdpo.server;
+
+/**
+ * Created by Stijak on 12.12.2015..
+ */
+public class ServerThread {
+}
