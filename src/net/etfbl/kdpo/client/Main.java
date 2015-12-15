f697f0a72ff31ee14dedc65e9679d31eb8414b54
ngolubovic <gvindolanOcube@gmail.com>
2015-12-15 21:16:17 +0100

    Hello World -> Hello World!

--- src/net/etfbl/kdpo/client/Main.java
+++ src/net/etfbl/kdpo/client/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
