9a85a934f9e38ef5b80b778f1614b4055b91dbd8
ngolubovic <gvindolanOcube@gmail.com>
2015-12-15 21:37:47 +0100

    Dodane klase od razgovora u subotu

---
+++ src/net/etfbl/kdpo/client/KDPOImage.java
@@ -0,0 +1,9 @@
+package net.etfbl.kdpo.client;
+
+/**
+ * Created by User on 12/15/2015.
+ *
+ * ne sjećam se koja nam je ideja bila sa ovim
+ */
+public class KDPOImage {
+}
---
+++ src/net/etfbl/kdpo/server/KeyGen.java
@@ -0,0 +1,7 @@
+package net.etfbl.kdpo.server;
+
+/**
+ * Created by User on 12/15/2015.
+ */
+public class KeyGen {
+}
