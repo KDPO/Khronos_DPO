172e54cc594782821fdd353750c63bebbeefcc97
ngolubovic <gvindolanOcube@gmail.com>
2015-12-15 21:45:27 +0100

    Dodane klase od razgovora u subotu

--- src/net/etfbl/kdpo/client/ImageFrame.java
+++ src/net/etfbl/kdpo/client/ImageFrame.java
@@ -1,7 +1,13 @@
 package net.etfbl.kdpo.client;
 
+import javafx.scene.image.Image;
+
 /**
  * Created by User on 12/15/2015.
  */
 public class ImageFrame {
+
+    Image image;
+    String name;
+
 }
