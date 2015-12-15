d185160fac9767796808d5417c791d9e5f84cfd9
markostijak <marko.stijak@gmail.com>
2015-12-16 00:00:21 +0100

    Pravimo konflikt

--- src/net/etfbl/kdpo/server/Server.java
+++ src/net/etfbl/kdpo/server/Server.java
@@ -4,7 +4,8 @@ package net.etfbl.kdpo.server;
  * Created by Stijak on 12.12.2015..
  */
 public class Server {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Ovo je commit od vladana!");
+        System.out.println("Nenad hoće da napravimo konflikt :D");
     }
 }
