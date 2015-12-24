58477958e9dadf6771159022b2a5c48bfc87e6fe
Marko Stijak <markostijak@users.noreply.github.com>
2015-12-24 19:06:45 +0100

    ObservableList umjesto ArrayList

--- src/main/java/net/etfbl/kdpo/client/VirtualAlbum.java
+++ src/main/java/net/etfbl/kdpo/client/VirtualAlbum.java
@@ -1,42 +1,43 @@
 package net.etfbl.kdpo.client;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.scene.image.Image;
+import javafx.collections.ObservableList;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
 
 /**
- * Created by User on 12/15/2015.
+ * Created by Stijak on 24.12.2015.
  */
 
 // kontejnerska klasa
 public class VirtualAlbum implements Serializable {
     private String name;
     private Date creationDate;
-    private ArrayList<File> images;
+    private ObservableList<File> images;
     private String description;
 
     public VirtualAlbum(String name, String description) {
-    }
-
-    public VirtualAlbum(String name, String description, ArrayList<File> images) {
-    }
-
-    public void removeImage(File file) {
-    }
-
-    public void addImage(File file) {
-    }
-
-    public void addImages(ArrayList<File> files) {
+        this.name = name;
+        this.description = description;
+        this.images = FXCollections.observableArrayList();
     }
 
     public void setStatusMessage(String message) {
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ObservableList<File> getImages() {
+        return images;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
 
